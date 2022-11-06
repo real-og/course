b95dff3eb01f349ff178f9f6c796b5decf17de67
Evgeny Grakovich <grakovich-e@yandex.by>
2022-11-06 23:05:45 +0300

    hide secret key

--- site.py
+++ site.py
@@ -6,13 +6,13 @@ import db
 
 # DATABASE = '/tmp/flsite.db'
 DEBUG = True
-SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
+SECRET_KEY = os.environ.get('secret_key')
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 #app.config.update(dict(DATABASE=os.path.join(app.root_path,'flsite.db')))
 
-# login_manager = LoginManager(app)
+login_manager = LoginManager(app)
 # login_manager.login_view = 'login'
 # login_manager.login_message = "Авторизуйтесь для доступа к закрытым страницам"
 # login_manager.login_message_category = "success"
